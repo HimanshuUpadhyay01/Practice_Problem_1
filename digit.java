7e4c15825fc8db73be95f8f6889e91d1d0aaf61d
HimanshuUpadhyay01 <hu7131@gmail.com>
2025-01-18 15:41:24 +0530

    first practice code

---
+++ digit.java
@@ -0,0 +1,37 @@
+public class digit{
+ public static void main(String [] args){
+  int num = 0;
+    for(num=1;num<=10;num++){
+   if(num==1){
+    System.out.println("One");
+   }
+    if(num==2){
+    System.out.println("Two");
+   }
+    if(num==3){
+    System.out.println("Three");
+   }
+    if(num==4){
+    System.out.println("Four");
+   }
+    if(num==5){
+    System.out.println("Five");
+   }
+    if(num==6){
+    System.out.println("Six");
+   }
+    if(num==7){
+    System.out.println("Seven");
+   }
+    if(num==8){
+    System.out.println("Eight");
+   }
+   if(num==9){
+    System.out.println("Nine");
+   }
+    if(num==10){
+    System.out.println("Ten");
+   } 
+ }
+}  
+}
---
+++ evenodd.java
@@ -0,0 +1,16 @@
+public class evenodd{
+ public static void main(String args[]){
+      System.out.println("even numbers from 1 to 10: ");
+      for(int i=1; i<=10;i++){
+          if(i%2==0){
+             System.out.println(i);
+          }
+      }
+      System.out.println("odd numbers from 1 to 10:");
+      for(int i=1; i<=10;i++){
+          if(i%2 != 0){
+               System.out.println(i);
+          }
+    }
+  }
+  }
